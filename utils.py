7c37bd6632b62a07b582ff591b487d435fe20c47
Sebastian Ranasinghe <sebastianranasinghe@Sebastians-MacBook-Air.local>
2025-01-04 16:48:13 -0500

    all_games_comb initial

---
+++ utils.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+def combine_team_games(df, keep_method='home'):
+    '''Combine a TEAM_ID-GAME_ID unique table into rows by game. Slow.
+
+        Parameters
+        ----------
+        df : Input DataFrame.
+        keep_method : {'home', 'away', 'winner', 'loser', ``None``}, default 'home'
+            - 'home' : Keep rows where TEAM_A is the home team.
+            - 'away' : Keep rows where TEAM_A is the away team.
+            - 'winner' : Keep rows where TEAM_A is the losing team.
+            - 'loser' : Keep rows where TEAM_A is the winning team.
+            - ``None`` : Keep all rows. Will result in an output DataFrame the same
+                length as the input DataFrame.
+                
+        Returns
+        -------
+        result : DataFrame
+    '''
+    # Join every row to all others with the same game ID.
+    joined = pd.merge(df, df, suffixes=['_H', '_A'],
+                      on=['SEASON_ID', 'GAME_ID', 'GAME_DATE'])
+    # Filter out any row that is joined to itself.
+    result = joined[joined.TEAM_ID_H != joined.TEAM_ID_A]
+    # Take action based on the keep_method flag.
+    if keep_method is None:
+        # Return all the rows.
+        pass
+    elif keep_method.lower() == 'home':
+        # Keep rows where TEAM_A is the home team.
+        result = result[result.MATCHUP_H.str.contains(' vs. ')]
+    elif keep_method.lower() == 'away':
+        # Keep rows where TEAM_A is the away team.
+        result = result[result.MATCHUP_H.str.contains(' @ ')]
+    elif keep_method.lower() == 'winner':
+        result = result[result.WL_H == 'W']
+    elif keep_method.lower() == 'loser':
+        result = result[result.WL_H == 'L']
+    else:
+        raise ValueError(f'Invalid keep_method: {keep_method}')
+    return result
+    
