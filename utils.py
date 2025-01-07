7547c95c8432483d1522adea0a2ed6e79286c68d
Sebastian Ranasinghe <sebastianranasinghe@sebastians-air.mynetworksettings.com>
2025-01-07 17:08:28 -0500

    started sql

--- utils.py
+++ utils.py
@@ -4,50 +4,47 @@ import sqlite3
 import os
 
 
-def create_database(db_path):
-    """Creates an SQLite database at the specified path."""
-    try:
-        conn = sqlite3.connect(db_path)
-        print(f"Database created successfully at: {db_path}")
-        conn.close()
-    except sqlite3.Error as e:
-        print(f"Error creating database: {e}")
+def alter_db(db_path, command):
 
-def create_table_from_df(db_path, table_name, df):
-    """Creates a table from a Pandas DataFrame.
+  try:
 
-    Args:
-        db_path: Path to the database.
-        table_name: Name of the table.
-        df: The Pandas DataFrame.
-    """
-    try:
-        conn = sqlite3.connect(db_path)
-        df.to_sql(table_name, conn, if_exists='replace', index=False)  # 'replace' drops table if it exists
-        conn.close()
-        print(f"Table '{table_name}' created from DataFrame successfully.")
-    except sqlite3.Error as e:
-        print(f"Error creating table from DataFrame: {e}")
+    sqliteConnection = sqlite3.connect(db_path)
+    cursor = sqliteConnection.cursor()
 
-def update_data(db_path, table_name, column_to_update, new_value, where_clause):
-    """Updates data in a table.
+    cursor.execute(command)
+    sqliteConnection.commit()
 
-    Args:
-        db_path: Database path.
-        table_name: Table name.
-        column_to_update: The column to update.
-        new_value: The new value for the column.
-        where_clause: The WHERE clause.
-    """
-    try:
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute(f"UPDATE {table_name} SET {column_to_update} = ? WHERE {where_clause}", (new_value,))
-        conn.commit()
-        conn.close()
-        print(f"Data updated in '{table_name}' successfully.")
-    except sqlite3.Error as e:
-        print(f"Error updating data: {e}")
+
+  except sqlite3.Error as e:
+      print(f"Database error: {e}")
+      return None
+
+  finally:
+    cursor.close()
+    sqliteConnection.close()
+
+
+def query_db(db_path, command):
+
+  try:
+
+    sqliteConnection = sqlite3.connect(db_path)
+    cursor = sqliteConnection.cursor()
+
+    cursor.execute(command)
+    result = cursor.fetchall()
+
+    return result
+
+
+  except sqlite3.Error as e:
+      print(f"Database error: {e}")
+      return None
+
+  finally:
+    cursor.close()
+    sqliteConnection.close()
+
 
 def combine_team_games(df, keep_method='home'):
     '''Combine a TEAM_ID-GAME_ID unique table into rows by game. Slow.
